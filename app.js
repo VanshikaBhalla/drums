a1935874182ceba90c2ec4af6797f2c537b65d55
Vanshika Bhalla <105324515+VanshikaBhalla@users.noreply.github.com>
2024-06-14 22:59:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.querySelectorAll(".drum").forEach((button)=>{
-    window.addEventListener("keydown", (e)=>{
+    window.addEventListener("keydown", (e)=>{    // check for keyboard press
         var b = button.innerText;
         if (e.key==b) {
             var a = new Audio(`${b}.mp3`);
@@ -7,7 +7,7 @@ document.querySelectorAll(".drum").forEach((button)=>{
         }
         e.preventDefault();
     });
-    button.addEventListener("click", ()=>{
+    button.addEventListener("click", ()=>{    // check for button click
         var a = new Audio(`${button.innerText}.mp3`);
         a.play();
     });
